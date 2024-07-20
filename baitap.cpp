3c0f501adbc25b5ad100e401f3d803887ada48dd
Nguyễn Công Đức <Duccongnguyen086@gmail.com>
2024-07-20 15:29:10 +0700

    Bai tap so hinh vuong +))

---
+++ baitap.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=10;
+ for (int i=1;i<=7;i++){
+ a=a+1;
+ cout<<a<<" ";}
+ cout<<endl;
+ a=20;0; 
+ for (int i=1;i<=7;i++){
+ 	a=a+1; 
+ if(a==21||a==22||a==27){
+ 	cout<<a<<" "; }
+ 	else {cout<<"   " ;
+	 } 
+ }
+ cout<<endl;
+ a=30; 
+ for (int i=1;i<=7;i++){
+ 	a=a+1; 
+ if(a==31||a==33||a==37){
+ 	cout<<a<<" "; 
+ } 
+ else{
+ 	cout<<"   "; 
+ } 
+ } cout<<endl;
+ a=40; 
+ for (int i=1;i<=7;i++){
+  a=a+1;
+  if(a==41||a==44||a==47){
+  	cout<<a<<" ";
+  } 
+  else {
+  	cout<<"   ";
+  }
+}
+  cout<<endl;
+a=50;
+for (int i=1;i<=7;i++){
+a=a+1;
+if(a==51||a==55||a==57){
+	cout<<a<<" ";
+} else {cout<<"   ";
+}
+
+}
+cout<<endl;
+a=60;
+  for (int i=1;i<=7;i++){
+  	a+=1;
+  	if(a==61||a==66||a==67){
+  		cout<<a<<" ";
+	  }
+	  else{cout<<"   ";
+	  }
+  }
+  cout<<endl;
+  a=70;
+  for (int i=1;i<=7;i++){
+  a+=1;
+   cout<<a<<" ";
+  }
+	return 0;
+}
